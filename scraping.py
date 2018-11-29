d1a4e778895025384ac4b3650a57a3a55d080197
nishikawwwa <snrg.164.ring@gmail.com>
2018-11-29 08:16:52 +0000

    mod scraping.py

--- scraping.py
+++ scraping.py
@@ -11,17 +11,43 @@ from pyvirtualdisplay import Display
 
 
 def each_page(id):
+
     URL = 'https://play.google.com/store/apps/details?id=' + id
     HTML = requests.get(url = URL)
-    soup = BeautifulSoup(HTML.content, "html.parser")
+    soup = BeautifulSoup(HTML.text, "lxml")
+    '''
+    with open(html_save + 'body.html', mode='w', encoding = 'utf-8') as f:
+        f.write(soup.prettify())
+    '''
     #category
     page_category = soup.find('meta', itemprop= 'applicationCategory')
     category = page_category.get('content')
     #website
-    #page_website = soup.find('div', class_='xyOfqd')
-    ss = soup.find(string = 'ウェブサイトにアクセス')
-    print(soup)
-    return ss
+    page_website = soup.find('div', class_='xyOfqd')
+    page_weball = page_website.findAll('div')
+
+    #真のweb url
+    ss = soup.find('a', string = 'Visit website')
+    if ss != None:
+        web_url = ss.get('href')
+
+    if ss == None:
+        web_url = None
+
+    print('ssssss=', web_url)
+    #住所
+    weball_size = len(page_weball)
+    page_address = page_weball[weball_size -1]
+    address = page_address.text
+    print('address ==',address)
+
+    #連絡先
+    page_mail = soup.find('a', class_ = 'hrTbp KyaTEc')
+    mail = page_mail.get('href')
+    mail_size = len(mail)
+    mail = mail[7:mail_size]
+    print('mail =', mail)
+    return web_url
 
 
 def page_collect():
@@ -41,7 +67,7 @@ def page_collect():
 
     soup = BeautifulSoup(html, "html.parser")
     page_r = soup.findAll('div', class_='card-content id-track-click id-track-impression')
-
+    #tex = each_page('com.sega.revolvers8')
     c = 0
     for count in page_r:
         #id
@@ -53,6 +79,7 @@ def page_collect():
         page_company = count.find('a', class_='subtitle')
         company = page_company.get('title')
         #each page
+        print(id)
         tex = each_page(id)
         print(c)
         #print(title)
